1b178c3526c6bb7f82a3cb724190ff90af47bc6d
norman lewis <takialog@gmail.com>
2018-03-07 12:18:32 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,9 +3,11 @@ class Song < ActiveRecord::Base
   belongs_to :genre
 
   def get_genre_name
+    self.name
   end
 
   def drake_made_this
     # when this method is called it should assign the song's artist to Drake
+    self.name = 'Drake'
   end
 end
